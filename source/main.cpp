9e4baed439d16a345d03ecdd5af1a7cdba801f34
Maschell <Maschell@gmx.de>
2020-06-07 13:59:35 +0200

    Make sure to call the init hook before the entrypoint.

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,7 @@
 #include <whb/log_udp.h>
 #include "function_patcher.h"
 WUMS_MODULE_EXPORT_NAME("homebrew_functionpatcher");
+WUMS_MODULE_INIT_BEFORE_ENTRYPOINT();
 
 WUMS_INITIALIZE(){
     WHBLogUdpInit();
