8827e89a8a800fa9f3cc74c245dd054d729f2068
Maschell <Maschell@gmx.de>
2023-01-04 22:34:03 +0100

    Log the module version on each application start

--- source/utils/globals.h
+++ source/utils/globals.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "../PatchedFunctionData.h"
+#include "version.h"
 #include <coreinit/memheap.h>
 #include <memory>
 #include <vector>
+
+#define MODULE_VERSION      "v0.1"
+#define MODULE_VERSION_FULL MODULE_VERSION MODULE_VERSION_EXTRA
 
 #define JUMP_HEAP_DATA_SIZE (32 * 1024)
 extern char gJumpHeapData[];
---
+++ source/version.h
@@ -0,0 +1,2 @@
+#pragma once
+#define MODULE_VERSION_EXTRA ""
