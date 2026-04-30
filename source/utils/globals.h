8cbd1a4a1819ca0bff9e41a40c2b6b7c65c93656
Maschell <Maschell@gmx.de>
2026-04-30 19:25:50 +0200

    Increase size of jump data heap

--- source/utils/globals.h
+++ source/utils/globals.h
@@ -9,7 +9,7 @@
 #define MODULE_VERSION      "v0.2.5"
 #define MODULE_VERSION_FULL MODULE_VERSION MODULE_VERSION_EXTRA
 
-#define JUMP_HEAP_DATA_SIZE (32 * 1024)
+#define JUMP_HEAP_DATA_SIZE (128 * 1024)
 extern char gJumpHeapData[];
 extern MEMHeapHandle gJumpHeapHandle;
 
